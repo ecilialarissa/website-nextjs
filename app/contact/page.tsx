2b45d9479d077ca8fe00b4809e16aa790629389a
Ecilia Larissa <ecilialarissa@gmail.com>
2024-06-24 13:09:03 -0300

    first commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 
 //pagina contato
-import Caroussel from "../components/Caroussel";
+
 import Header from "../components/Header";
 
 
@@ -8,7 +8,7 @@ export default function Contact () {
 
     return(
     <>
-    <Caroussel/>
+
     <Header/>
 
     </>
